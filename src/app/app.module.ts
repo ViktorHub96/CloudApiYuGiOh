2f1b96cc125307c67ab5fc287706052af75f5da0
Viktor Pera <peviktor@hotmail.com>
2019-05-09 13:03:28 +0200

    Layout
    Filter maken voor welke kaarten je wilt weergeven

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -8,13 +7,14 @@ import { YuGiOhCardsComponent } from './yu-gi-oh-cards/yu-gi-oh-cards.component'
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import {ButtonModule} from 'primeng/button';
-//import {MenuItem} from 'primeng/api';
+import {DropdownModule} from 'primeng/dropdown';
 import {AccordionModule} from 'primeng/accordion';
 import {BreadcrumbModule} from 'primeng/breadcrumb';
-
+import {InputTextModule} from 'primeng/inputtext';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { CardDetailsComponent } from './card-details/card-details.component';
 import { MenuToolBarComponent } from './menu-tool-bar/menu-tool-bar.component';  
+import { FormsModule} from "@angular/forms"
 
 @NgModule({
   declarations: [
@@ -28,9 +28,12 @@ import { MenuToolBarComponent } from './menu-tool-bar/menu-tool-bar.component';
   ],
   imports: [
     BrowserModule,
+    DropdownModule,
     BrowserAnimationsModule,
     ButtonModule,
     BreadcrumbModule,
+    FormsModule,
+    InputTextModule,
     AccordionModule,
     AppRoutingModule,
     HttpClientModule,
--- src/app/yu-gi-oh-cards/yu-gi-oh-cards.component.ts
+++ src/app/yu-gi-oh-cards/yu-gi-oh-cards.component.ts
@@ -1,18 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import {SelectItem} from 'primeng/api';
 
+interface City {
+  name: string;
+  code: string;
+}
 
 @Component({
   selector: 'app-yu-gi-oh-cards',
   templateUrl: './yu-gi-oh-cards.component.html',
   styleUrls: ['./yu-gi-oh-cards.component.css']
 })
+
+
 export class YuGiOhCardsComponent implements OnInit {
-
+  
   public result: any = [];
+  public optie: City[];
+  public Options: City;
 
-  constructor(private http: HttpClient,private router: Router) { }
+  constructor(private http: HttpClient,private router: Router) {
+    this.optie = [
+      {name: 'Search by Card Name', code: 'CN'},
+      {name: 'Search by Card Text', code: 'CT'},
+      {name: 'Search by Pendulum Effect', code: 'PE'},
+      {name: 'Search by Card Number', code: 'CNU'},
+     
+    ];
+   }
 
   ngOnInit() {
 
